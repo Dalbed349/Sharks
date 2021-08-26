0b9804e0f9095487c492a7464ef902c4f1827fc3
Dallas <albed349@newschool.edu>
2021-08-26 12:09:29 -0400

    edits

--- main.js
+++ main.js
@@ -21,9 +21,19 @@ console.log(xScale.range());
 console.log(yScale.domain())
 //Axis
         const yAxis = d3.axisLeft(yScale);
-        const xAxis = d3.axisBottom(xScale).tickValues(["1758","1841"]);
+        const xAxis = d3.axisBottom(xScale).tickValues(["1980","1994","2008","2016","2021"]);
 
         
+        var XaxisData = data.map(function(d) { return +d.Year; });
+        var YaxisData = data.map(function(d) { return +d.NewSpecies; });
+        regression=leastSquaresequation(XaxisData,YaxisData);
+        console.log(XaxisData)
+        console.log(YaxisData)
+        var line = d3.line()
+            .x(function(d) { return xScale(d.Year); })
+            .y(function(d) { return yScale(regression(d.Year)); });     
+        
+
 //Append Axis
     svg.append("g")
     .attr("transform", `translate(${margin.left},${innerHeight})`)
@@ -32,7 +42,15 @@ console.log(yScale.domain())
         .attr("transform", "translate(-10,0)rotate(-90)")
         .style("text-anchor", "end")
         .attr("x", -10);
-    
+
+        svg.append("path")
+        .attr("transform", `translate(${margin.left},0)`)
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line)
+        .style("stroke", "red") 
+        .attr("stroke-width",10)
+
      svg.append("g")
         .call(yAxis)
         .attr("transform", `translate(${margin.left})`);
@@ -59,7 +77,46 @@ console.log(yScale.domain())
                 document.getElementById('message').textContent = message;
                 $("#ex1").html(JSON.stringify(data, null, 3));
                 ///
+                var filteredData = data.filter(function(d) 
+                { 
+                    if( d["Year"] >= 1980
+                        // if( d["Year"] >= 1980 && d["NewSpecies"] <=60 
+                        ) 
+                        { 
+                            return d;
+                        } 
+                
+                    })
+
+
                 console.log(d3.max(data, d => +d.NewSpecies))
-                render(data)
+                // render(data)
+                render(filteredData)
             }
             )
+
+            function leastSquaresequation(XaxisData, Yaxisdata) {
+                var ReduceAddition = function(prev, cur) { return prev + cur; };
+                
+                // finding the mean of Xaxis and Yaxis data
+                var xBar = XaxisData.reduce(ReduceAddition) * 1.0 / XaxisData.length;
+                var yBar = Yaxisdata.reduce(ReduceAddition) * 1.0 / Yaxisdata.length;
+            
+                var SquareXX = XaxisData.map(function(d) { return Math.pow(d - xBar, 2); })
+                  .reduce(ReduceAddition);
+                
+                var ssYY = Yaxisdata.map(function(d) { return Math.pow(d - yBar, 2); })
+                  .reduce(ReduceAddition);
+                  
+                var MeanDiffXY = XaxisData.map(function(d, i) { return (d - xBar) * (Yaxisdata[i] - yBar); })
+                  .reduce(ReduceAddition);
+                  
+                var slope = MeanDiffXY / SquareXX;
+                var intercept = yBar - (xBar * slope);
+                
+            // returning regression function
+                return function(x){
+                  return x*slope+intercept
+                }
+            
+              }
